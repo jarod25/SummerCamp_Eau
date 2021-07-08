b56fbf6dd608cbd70661a7a578ac3ab0004b6c8c
jarod25 <63459117+jarod25@users.noreply.github.com>
2021-07-08 15:28:45 +0200

    Update index.php

--- Cancer/index.php
+++ Cancer/index.php
@@ -1,6 +1,6 @@
 <?php
 header ("content-type: image/png");
-?>
+
 $image = imagecreate(1000, 1000);
 imagepng($image);
-$background_color = imagecolorallocate($image, ?, ?, ?);
+$background_color = imagecolorallocate($image, 172, 29, 221);
